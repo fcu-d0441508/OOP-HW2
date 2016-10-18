5a263c0ac2161002d2149b33dcb0cc04fe50d055
d0441508 <a0930152994@gmail.com>
2016-10-18 23:44:54 +0800

    +

--- src/lab2/Break.java
+++ src/lab2/Break.java
@@ -1,9 +1,13 @@
 
 public class Break {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		for(int i = 0; i < 5; i ++) 
+		{
+			if(i==0||i==1)
+			System.out.println(i);
+			
+			else
+				break;
+		}
+		}
 }
